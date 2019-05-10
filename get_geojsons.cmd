7086dd9e91048669506c064e723ec3161404c5cf
morkapronczay <mor.kapronczay@gmail.com>
2019-05-10 17:37:31 +0200

    initial commit

---
+++ get_geojsons.cmd
@@ -0,0 +1,11 @@
+rem THAILAND
+curl -f -o data/thailand.zip --url "https://wambachers-osm.website/boundaries/exportBoundaries?cliVersion=1.0&cliKey=71d58a4f-a683-4d82-a1f6-6c7924751617&exportFormat=json&exportLayout=levels&exportAreas=land&from_al=4&to_al=4&union=false&selected=THA"
+
+rem LAOS
+curl -f -o data/laos.zip --url "https://wambachers-osm.website/boundaries/exportBoundaries?cliVersion=1.0&cliKey=71d58a4f-a683-4d82-a1f6-6c7924751617&exportFormat=json&exportLayout=levels&exportAreas=land&from_al=4&to_al=4&union=false&selected=LAO"
+
+rem CAMBODIA
+curl -f -o data/cambodia.zip --url "https://wambachers-osm.website/boundaries/exportBoundaries?cliVersion=1.0&cliKey=71d58a4f-a683-4d82-a1f6-6c7924751617&exportFormat=json&exportLayout=levels&exportAreas=land&from_al=4&to_al=4&union=false&selected=KHM"
+
+rem MALAYSIA
+curl -f -o data/malaysia.zip --url "https://wambachers-osm.website/boundaries/exportBoundaries?cliVersion=1.0&cliKey=71d58a4f-a683-4d82-a1f6-6c7924751617&exportFormat=json&exportLayout=levels&exportAreas=land&from_al=4&to_al=4&union=false&selected=MYS"
